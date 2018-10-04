7d2ec2872d34c48ccc5fbe7261f92fdccc678df4
CodyBagwandeen <cbagwandeen@stuy.edu>
2018-10-04 16:26:53 -0400

    updated

--- Driver.java
+++ Driver.java
@@ -1,6 +1,7 @@
 public class Driver {
   public static void main(String[] args){
     BankAccount a = new BankAccount( 100.00, 101, "pass");
+    BankAccount b = new BankAccount ( 500.72, 201, "word");
 
     System.out.println( a.getAccount());
     System.out.println( a.getBalance());
@@ -8,6 +9,8 @@ public class Driver {
     System.out.println(a.getPassword());
     a.setPassword( "I think that pineapple on pizza isn't the worst thing ever");
     System.out.println(a.getPassword());
+    a.setPassword( "pass");
+
 
     System.out.println(a.deposit( 50.1 ));
     System.out.println(a.getBalance());
@@ -15,6 +18,15 @@ public class Driver {
     System.out.println(a.getBalance());
     System.out.println(a.withdraw(70.5));
     System.out.println(a.getBalance());
+
+    System.out.println( b.transferTo( a, 50.2, "pass"));
+    System.out.println( a.getBalance());
+    System.out.println( b.getBalance());
+    System.out.println( a.transferTo( b, 50.2, "pass"));
+    System.out.println( a.getBalance());
+    System.out.println( b.getBalance());
+
+
 
 
   }
